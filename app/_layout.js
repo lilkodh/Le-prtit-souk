6d10e3ee9d83821afc1d98dd1defd9d0bddf479e
lilkodh <khaliddrihemdev@gmail.com>
2026-05-22 16:39:27 +0100

    Style tab bar icons with rounded border and active state

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,35 @@
 import { Tabs } from "expo-router";
+import { View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 export default function Layout (){
   return(
     <Tabs  screenOptions={{
         headerShown: false,tabBarStyle: {
-  backgroundColor: "#06251B",height: 85 ,borderColor: "#ECC880",
+  backgroundColor: "#06251B",height: 85 ,borderColor: "#D4A84F",
 },
       }}>
-      <Tabs.Screen name="index" />
+      <Tabs.Screen name="index" options={{
+        tabBarIcon :({color , name , size ,  focused}) => (
+         <View style= {{
+           borderWidth: 2,
+height:30,
+width:60,
+    borderColor: "#D4A84F",
+    borderRadius: 20,
+    backgroundColor: focused
+      ? "red"
+      : "transparent",
+         }}>
+          <Ionicons 
+          name="home"
+          size={25}
+          
+          />
+          </View>
+         
+        ),
+      }} />
           
     </Tabs>
   );
